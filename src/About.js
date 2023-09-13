3ba5d448045bbc191d4bdc119ffa8b55d5411728
windows <windows@microsoft.com>
2023-09-13 19:38:36 +0530

    create project

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Myfunc1()
+{
+    return <h1>welcome to react js</h1>
+}
+
+export default Myfunc1
+
+
+export function Myfunc2()
+{
+    return <h2>this  is name export function </h2>
+}
