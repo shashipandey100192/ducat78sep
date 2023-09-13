f68c62bae90f8acf6e8d88b933c109282ff35df6
windows <windows@microsoft.com>
2023-09-13 19:55:40 +0530

    change component

---
+++ src/Contactus.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Contactus() {
+  return (
+    <div>Contactus using git access </div>
+  )
+}
+
+export default Contactus
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Myfunc1,{Myfunc2} from './About';
+import Contactus from './Contactus';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -8,6 +9,7 @@ root.render(
   <React.StrictMode>
     <Myfunc1/>
     <Myfunc2/>
+    <Contactus/>
   </React.StrictMode>
 );
 
